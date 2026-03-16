63e2995fc28f0aa2781064bd4115f6b5bcabee96
Kudrin Dmitry <vanabsten2@gmail.com>
2026-03-16 17:53:32 +0100

    Initial Commit

---
+++ ClaudePromo/ClaudePromoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaudePromoApp.swift
+//  ClaudePromo
+//
+//  Created by Dmitriy Kudrin on 16.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct ClaudePromoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
